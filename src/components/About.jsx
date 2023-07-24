7b41782650c3b7a8a79e330cd260060e9d8ffe26
Dora Do <dora.thien.do@gmail.com>
2023-07-24 17:04:16 -0400

    update favicon and about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
                 <div>
                     <h1 className={styles.h1}>About Me</h1>
                     <p className={styles.paragraph}>
-                        I started my journey into technology at a young age. Growing up in the Silicon Valley and raised engineers, I had a unique childhood that embraced using the latest technology and theorizing the future of it.
+                        I started my journey into technology at a young age. Growing up in the Silicon Valley and raised by engineers, I had a unique childhood that embraced using the latest technology and theorizing the future of it.
                     </p>
                     <p>
                         After attaining my B.S. degree in Computer Science at San Jose State University, I worked as a software engineer at Macys.com in San Francisco, where I fell in love with front-end engineering and design. I then worked at Kodak Moments, leading a team of front-end engineers to build a web-based shopping experience on the Kodak Moments website. More recently, I moved to New York City and transitioned into a design technologist role at Square Inc., where I worked with the marketing team to build interactive web experiences for Square's products and campaigns.
